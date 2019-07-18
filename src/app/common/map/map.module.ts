b98c894a58be044ba0f2b5e7925227a2bc7bc822
bval73 <52051327+bval73@users.noreply.github.com>
2019-07-18 08:53:21 -0400

    Add map and component google api

---
+++ src/app/common/map/map.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { MapComponent } from './map.component';
+import { AgmCoreModule } from '@agm/core';
+import { CommonModule } from '@angular/common';
+
+import { CamelizePipe } from 'ngx-pipes';
+
+
+import { MapService } from './map.service';
+
+//google map key
+//AIzaSyD-tsuo3-7P6MHm1ehk53rauJzav_OfhPM
+
+@NgModule({
+  declarations: [
+    MapComponent
+  ],
+  exports:[
+    MapComponent
+  ],
+  imports: [
+    AgmCoreModule.forRoot({
+        apiKey: 'AIzaSyD-tsuo3-7P6MHm1ehk53rauJzav_OfhPM'
+    }),
+    CommonModule
+  ],
+  providers: [
+    MapService,
+    CamelizePipe
+  ],
+
+})
+
+export class MapModule {
+    lat: number = 51.678418;
+    lng: number = 7.809007;
+
+ }
